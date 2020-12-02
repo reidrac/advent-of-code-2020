65a784e65baccb6a9fb1d5af7c0d75dd6cae5cbc
Juan J. Martinez <jjm@usebox.net>
2020-12-02 19:34:42 +0000

    Using matchers

--- advent/test/src/DayOneSpec.scala
+++ advent/test/src/DayOneSpec.scala
@@ -1,8 +1,9 @@
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
 import scala.io.Source
 
-class DayOneSpec extends AnyFlatSpec {
+class DayOneSpec extends AnyFlatSpec with Matchers {
   val in =
     Source
       .fromResource("1.txt")
@@ -11,11 +12,11 @@ class DayOneSpec extends AnyFlatSpec {
       .toSeq
 
   "Report Repair" should "find the two entries that sum to 2020" in {
-    assert((for {
+    (for {
       a <- in
       b <- in
       if a != b && a + b == 2020
-    } yield a * b).headOption == Some(866436))
+    } yield a * b).headOption shouldBe Some(866436)
   }
 
   it should "find the three entries that sum to 2020" in {
@@ -24,6 +25,6 @@ class DayOneSpec extends AnyFlatSpec {
       b <- in
       c <- in
       if a != b && a != c && b != c && a + b + c == 2020
-    } yield a * b * c).headOption == Some(276650720)
+    } yield a * b * c).headOption shouldBe Some(276650720)
   }
 }
--- advent/test/src/DayTwoSpec.scala
+++ advent/test/src/DayTwoSpec.scala
@@ -1,8 +1,9 @@
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
 import scala.io.Source
 
-class DayTwoSpec extends AnyFlatSpec {
+class DayTwoSpec extends AnyFlatSpec with Matchers {
 
   val in =
     Source
@@ -19,35 +20,29 @@ class DayTwoSpec extends AnyFlatSpec {
     }
 
   "Password Philosophy" should "check how many paswords match corporate policy" in {
-    assert(
-      in.map(extract)
-        .map {
-          case (from, to, letter, password) =>
-            (from, to, password.count(_ == letter))
-        }
-        .filter {
-          case (from, to, count) =>
-            count >= from && count <= to
-        }
-        .length == 556
-    )
+    in.map(extract)
+      .map {
+        case (from, to, letter, password) =>
+          (from, to, password.count(_ == letter))
+      }
+      .filter {
+        case (from, to, count) =>
+          count >= from && count <= to
+      } should have length 556
   }
 
   it should "check how many paswords match corporate policy (position)" in {
-    assert(
-      in.map(extract)
-        .filter {
-          case (pos1, pos2, _, password) =>
-            pos1 <= password.length && pos2 <= password.length
-        }
-        .filter {
-          case (pos1, pos2, letter, password) =>
-            (password.charAt(pos1 - 1) == letter && password
-              .charAt(pos2 - 1) != letter) || (password
-              .charAt(pos1 - 1) != letter && password
-              .charAt(pos2 - 1) == letter)
-        }
-        .length == 605
-    )
+    in.map(extract)
+      .filter {
+        case (pos1, pos2, _, password) =>
+          pos1 <= password.length && pos2 <= password.length
+      }
+      .filter {
+        case (pos1, pos2, letter, password) =>
+          (password.charAt(pos1 - 1) == letter && password
+            .charAt(pos2 - 1) != letter) || (password
+            .charAt(pos1 - 1) != letter && password
+            .charAt(pos2 - 1) == letter)
+      } should have length 605
   }
 }
